1a9829b48ab5e94d0b7bb76d5b04383acfe00f27
Aiswarya-jayesh <Aiswarya.KJ@ompventure.com>
2026-03-25 18:41:19 +0530

    build changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    minify: 'esbuild',        // or 'terser' if you prefer
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // adjust to your heavy deps
+        },
+      },
+    },
+  },
 })
